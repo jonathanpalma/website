9fbcf06ae323dfb1eff668ba6d8b7865e08f9a32
Jonathan Palma <tanpalma04@gmail.com>
2019-03-04 16:40:25 -0600

    Updated stylesheets

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,20 +9,15 @@ const App = () => (
   <Fragment>
     <SiteMetadata />
     <div className="content">
-      <h1 className="title">Jonathan Palma</h1>
+      <h1 className="title">Jonathan <span className="last-name">Palma</span></h1>
       <h2 className="subtitle">Full Stack Developer</h2>
       <span className="separator" />
-      <p>
+      <p className="bio">
         Hi! I am Jonathan, a full stack developer with over 5 years of
-        {' '}
         experience based in El Salvador, Central America. I have worked
-        {' '}
         for startups, mid-sized and multinational companies, and collaborated
-        {' '}
         with talented people to create digital products for both business and
-        {' '}
         consumer use. I'm quietly confident and passionate about what I do and
-        {' '}
         I find happiness in dancing, surfing, coding and looking for new challenges.
       </p>
       <Menu>
