9194f1ed04db841e2c05463a3b4717a47f175c16
Jonathan Palma <tanpalma04@gmail.com>
2019-11-04 14:57:13 -0600

    refactor: update bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,16 +9,18 @@ const App = () => (
   <Fragment>
     <SiteMetadata />
     <div className="content animated fadeIn">
-      <h1 className="title">Jonathan <span className="last-name">Palma</span></h1>
-      <h2 className="subtitle">Full Stack Developer</h2>
+      <h1 className="title">
+        Jonathan <span className="last-name">Palma</span>
+      </h1>
+      <h2 className="subtitle">Software Engineer</h2>
       <span className="separator" />
       <p className="bio">
-        Hi! I am Jonathan, a full stack developer with over 5 years of
-        experience based in El Salvador, Central America. I have worked
-        for startups, mid-sized and multinational companies, and collaborated
-        with talented people to create digital products for both business and
-        consumer use. I'm quietly confident and passionate about what I do and
-        I find happiness in dancing, surfing, coding and looking for new challenges.
+        Hi! I am Jonathan, a software engineer with over 5 years of experience
+        based in El Salvador, Central America. I have worked for startups,
+        mid-sized and multinational companies, and collaborated with talented
+        people to create digital products for business and consumer use. I'm
+        quietly confident and passionate about what I do and I find happiness in
+        dancing, hiking, surfing, and releasing open source code.
       </p>
       <Menu>
         <Menu.Option
@@ -40,7 +42,8 @@ const App = () => (
         <Menu.Option
           icon="file-text"
           label="Resume"
-          url="#"
+          download="Resume_JonathanPalma.pdf"
+          url="/public/resume.pdf"
         />
       </Menu>
     </div>
