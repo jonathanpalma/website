98d4b1f17b79a324f3ecd08597a191aa42eccca7
Jonathan Palma <tanpalma04@gmail.com>
2020-04-25 03:32:49 -0600

    update email

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,27 @@
-import React, { Fragment } from 'react';
-import SiteMetadata from 'components/SiteMetadata';
-import Menu from 'components/Menu';
+import React, { Fragment } from "react"
+import SiteMetadata from "components/SiteMetadata"
+import Menu from "components/Menu"
 
-import 'css/index.css';
-import 'css/font-awesome.min.css';
+import "css/index.css"
+import "css/font-awesome.min.css"
 
 const App = () => (
   <Fragment>
     <SiteMetadata />
     <div className="content animated fadeIn">
-      <h1 className="title">Jonathan <span className="last-name">Palma</span></h1>
+      <h1 className="title">
+        Jonathan <span className="last-name">Palma</span>
+      </h1>
       <h2 className="subtitle">Full Stack Developer</h2>
       <span className="separator" />
       <p className="bio">
         Hi! I am Jonathan, a full stack developer with over 5 years of
-        experience based in El Salvador, Central America. I have worked
-        for startups, mid-sized and multinational companies, and collaborated
-        with talented people to create digital products for both business and
-        consumer use. I'm quietly confident and passionate about what I do and
-        I find happiness in dancing, surfing, coding and looking for new challenges.
+        experience based in El Salvador, Central America. I have worked for
+        startups, mid-sized and multinational companies, and collaborated with
+        talented people to create digital products for both business and
+        consumer use. I'm quietly confident and passionate about what I do and I
+        find happiness in dancing, surfing, coding and looking for new
+        challenges.
       </p>
       <Menu>
         <Menu.Option
@@ -35,16 +38,12 @@ const App = () => (
           icon="envelope"
           label="Contact"
           target="_top"
-          url="mailto:tanpalma04@gmail.com"
+          url="mailto:jonathanpalma.me@gmail.com"
         />
-        <Menu.Option
-          icon="file-text"
-          label="Resume"
-          url="#"
-        />
+        <Menu.Option icon="file-text" label="Resume" url="#" />
       </Menu>
     </div>
   </Fragment>
-);
+)
 
-export default App;
+export default App
