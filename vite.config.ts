f34bc338a86740624af77d467e43251e8bd02594
Jonathan Palma <jonathanpalma.me@gmail.com>
2024-06-23 14:18:46 -0600

    feat: add remix devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { remixDevTools } from "remix-development-tools";
 import { flatRoutes } from "remix-flat-routes";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    remixDevTools(),
     remix({
       future: {
         v3_fetcherPersist: true,
